7fe16a3c0992caf911c303020c82fab4b7b62b7a
Areltr <arelanjay528@gmail.com>
2025-01-19 13:33:33 +0000

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,77 @@
-var div = document.getElementById('host');
-var display = 1;
+const exchangeRate = {
+    usd: 0.000065,
+    myr:0.00027
+};
 
-function hideShow()
-{
-    if(display == 1)
-    {
-        div.style.display = 'block';
-        display = 0;
-    }
-    else
-    {
-        div.style.display = 'none';
-        display = 1;
-    }
-}
-var p = document.getElementById('srv');
-var display = 1;
 
-function hideShow1()
-{
-    if(display == 1)
-    {
-        p.style.display = 'block';
-        display = 0;
-    }
-    else
-    {
-        p.style.display = 'none';
-        display = 1;
-    }
-}
-var p = document.getElementsByClassName('shr')
-var display = 1;
 
-function shr()
-{
-    if(display == 1)
-    {
-        p.style.display = 'block';
-        display = 0;
+const dropdownMenu = document.getElementById('dropdownMenu');
+const mainButton = document.getElementById('mainButton');
+
+window.onload = function () {
+    covertCurrency('idr');
+};
+
+mainButton.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('show');
+});
+
+function covertCurrency(currency) {
+    mainButton.textContent = currency.charAt(0).toUpperCase() + currency.slice(1);
+    const prices = document.querySelectorAll('.price');
+    const perMonth = document.querySelectorAll('.per-month');
+    
+    prices.forEach((price, index) =>{
+        const rupiahValue = parseFloat(price.dataset.rupiah);
+
+        if (currency === 'usd') {
+            const usdValue = (rupiahValue * exchangeRate.usd).toFixed(2);
+            price.textContent = `$ ${usdValue} /`;
+            perMonth[index].textContent = 'monthly';
+        } else if (currency === 'myr') {
+            const myrValue = (rupiahValue * exchangeRate.myr).toFixed(2);
+            price.textContent = `RM ${myrValue} /`;
+            perMonth[index].textContent = 'bulan';
+        } else if (currency === 'idr') {
+            price.textContent = `Rp. ${rupiahValue.toLocaleString('id-ID')} /`;
+            perMonth[index].textContent = 'bulan';
+        }
+    });
+    
+    dropdownMenu.classList.remove('show');
+};
+
+window.addEventListener('click', (event) => {
+    if (!dropdownMenu.contains(event.target)) {
+        dropdownMenu.classList.remove('show');
     }
-    else
-    {
-        p.style.display = 'none';
-        display = 1;
+});
+
+const dropdownButton = document.getElementById('dropdownButton');
+const dropdown = document.querySelector('.dropdown2');
+
+window.onload = function () {
+    showContent('mob');
+};
+
+
+dropdownButton.addEventListener('click', () => {
+    dropdown.classList.toggle('show');
+});
+
+function showContent(category){
+    dropdownButton.textContent = category.charAt(0).toUpperCase() + category.slice(1);
+
+    document.querySelectorAll('.content').forEach(content => {
+        content.classList.remove('active')});
+    
+    document.getElementById(category).classList.add('active');
+
+    dropdown.classList.remove('show');
+};
+
+window.addEventListener('click', (event) => {
+    if (!dropdown.contains(event.target)) {
+        dropdown.classList.remove('show');
     }
-}
+})
